06abef5886b13e69d14b4cc009544bfda7cb8966
JoaoLSouto <joaolucas0810@gmail.com>
2023-02-14 18:51:03 -0300

    Route private Fixs

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {useContext} from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
  
-import { AuthProvider } from "./contexts/auth";
+import { AuthProvider, AuthContext } from "./contexts/auth";
 
 const AppRoutes = () => {
+    const Private = ({children}) => {
+        const {authenticaded} = useContext(AuthContext);
+
+        if(!authenticaded) {
+            return <Navigate to="/login"/>
+        }
+        return children;
+    };
     return (
         <BrowserRouter>
         <AuthProvider>
 
             <Routes>
-                <Route path="/" element={<HomePage/>} />
                 <Route path="/login" element={<LoginPage/>} />
+                <Route path="/" element={<Private><HomePage/></Private>} />
             </Routes>
         </AuthProvider>
         </BrowserRouter>
