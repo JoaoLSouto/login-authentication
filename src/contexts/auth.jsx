db095b439d23b84c9b68c9699b3a5a229965aaa2
JoaoLSouto <contato.joaolsouto@gmail.com>
2023-02-15 20:12:40 -0300

    API services fixs.

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,12 +1,14 @@
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate} from "react-router-dom";
-
+import {api, createSession} from "../services/api"
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
+
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const recoveredUser = localStorage.getItem(['user']);
 
@@ -16,32 +18,35 @@ export const AuthProvider = ({children}) => {
         setLoading(false);
     }, []);
 
-    
-
-    const login = (email, password) => {
+    const login = async (email, password) => {
         console.log("login auth", {email, password});
+        const response = await createSession(email, password);
 
+        console.log("login", response.data);
         // api criar uma session
 
-        const loggedUser= {
-            id: "123",
-            email,
-        };
+        const loggedUser= response.data.user; 
+        const token = response.data.token;
 
         localStorage.setItem("user", JSON.stringify(loggedUser));
+        localStorage.setItem("token", token);
+        
+        api.defaults.headers.Authorization = `Bearer  ${token}`;
 
-        if(password === "senha"){
             setUser(loggedUser);
             navigate("/");
-        }
+        
 
         setUser({ id:"123", email});
     };
 
     const logout = () => {
         console.log("logout");
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
         setUser(null);
         navigate("/login");
+        api.defaults.headers.Authorization = null;
     };
 
     return (
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,15 +1,42 @@
-import React, { useContext} from "react";
+import React, { useContext, useState, useEffect} from "react";
 import { AuthContext } from "../../contexts/auth";
+import {getUsers} from "../../services/api";
+
 const HomePage = () => {
     const { logout, authenticaded } = useContext(AuthContext);
+    const [users, setUsers] = useState ([]);
+    const [loading, setLoading] = useState (true);
+
+    useEffect(() => {
+        (async () => {
+            const response = await getUsers();
+            setUsers(response.data);
+            setLoading(false);
+
+        })();
+    }, []);
+
     const handleLogout = () => {
         logout();
     }
+    if(loading) {
+        return <div className="loading">Carregando dados...</div>
+    }
+
     return (
     <>
         <h1>HomePage</h1>
         <p>{String(authenticaded)}</p>
         <button onClick={handleLogout}>Logout</button>    
+        <ul>
+            {
+                users.map((user) => (
+                    <li>
+                        {user._id} - {user.email}
+                    </li>
+                ))
+            }
+        </ul>
     </>
     );
 };
